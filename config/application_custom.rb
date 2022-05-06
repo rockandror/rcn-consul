c75631f4b429d2af060cecfcf196f759743867ae
Senén Rodero Rodríguez <senenrodero@gmail.com>
2022-05-06 13:32:30 +0200

    Change application languages
    And change the default language to Norwegian.

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,10 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.available_locales = ["en", "nn"]
+    config.i18n.default_locale = :nn
+    config.i18n.fallbacks = {
+      "en" => "en",
+      "nn" => "en"
+    }
   end
 end
