javierm/consul

Add description to poll questions translations
So we can show it as a hint for the question when defined.

We needed to recall `globalize_accessors` from the
custom `Poll::Question` model. Otherwise the application fails
when calling the globalize accessor for the new `description`
translatable field. The weirdest thing is that only fails in the
test `spec/system/admin/translatable_spec.rb:389`. It can be
reproduced locally and when running CI. The application
seems to work fine in the development environment

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_27_093019) do
+ActiveRecord::Schema.define(version: 2022_03_27_103314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -1125,6 +1125,7 @@ ActiveRecord::Schema.define(version: 2022_03_27_093019) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.datetime "hidden_at"
+    t.text "description"
     t.index ["hidden_at"], name: "index_poll_question_translations_on_hidden_at"
     t.index ["locale"], name: "index_poll_question_translations_on_locale"
     t.index ["poll_question_id"], name: "index_poll_question_translations_on_poll_question_id"

rockandror/rcn-consul

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_22_155419) do
+ActiveRecord::Schema.define(version: 2022_08_22_155714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -1126,6 +1126,7 @@ ActiveRecord::Schema.define(version: 2022_08_22_155419) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.datetime "hidden_at"
+    t.text "description"
     t.index ["hidden_at"], name: "index_poll_question_translations_on_hidden_at"
     t.index ["locale"], name: "index_poll_question_translations_on_locale"
     t.index ["poll_question_id"], name: "index_poll_question_translations_on_poll_question_id"