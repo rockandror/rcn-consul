javierm/consul

Make questions optional

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_23_122206) do
+ActiveRecord::Schema.define(version: 2022_03_27_093019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -1141,6 +1141,7 @@ ActiveRecord::Schema.define(version: 2022_03_23_122206) do
     t.datetime "updated_at"
     t.tsvector "tsv"
     t.string "video_url"
+    t.boolean "mandatory_answer", default: false, null: false
     t.index ["author_id"], name: "index_poll_questions_on_author_id"
     t.index ["poll_id"], name: "index_poll_questions_on_poll_id"
     t.index ["proposal_id"], name: "index_poll_questions_on_proposal_id"

rockandror/rcn-consul

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_22_124738) do
+ActiveRecord::Schema.define(version: 2022_08_22_155419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -1142,6 +1142,7 @@ ActiveRecord::Schema.define(version: 2022_08_22_124738) do
     t.datetime "updated_at"
     t.tsvector "tsv"
     t.string "video_url"
+    t.boolean "mandatory_answer", default: false, null: false
     t.index ["author_id"], name: "index_poll_questions_on_author_id"
     t.index ["poll_id"], name: "index_poll_questions_on_poll_id"
     t.index ["proposal_id"], name: "index_poll_questions_on_proposal_id"